2d514bd379d60ab193555b3615912ead537e3001
nitinchalopadho <nitin@chalopadho.com>
2019-07-26 23:46:15 +0530

    Updates

---
+++ precache-manifest.fefe47551853ab3cb6541716cf218a63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d63ce7c04253f721dc86a706577129",
+    "url": "/react-ant/index.html"
+  },
+  {
+    "revision": "73c80fe9d836568db13d",
+    "url": "/react-ant/static/css/2.cb759328.chunk.css"
+  },
+  {
+    "revision": "a59381a33afe588c6c72",
+    "url": "/react-ant/static/css/main.0eee9a63.chunk.css"
+  },
+  {
+    "revision": "73c80fe9d836568db13d",
+    "url": "/react-ant/static/js/2.e91a4358.chunk.js"
+  },
+  {
+    "revision": "a59381a33afe588c6c72",
+    "url": "/react-ant/static/js/main.314fb40f.chunk.js"
+  },
+  {
+    "revision": "33f4303f0bdd6998a845",
+    "url": "/react-ant/static/js/runtime~main.b9774cc2.js"
+  },
+  {
+    "revision": "b9deea1eb151ea315080dee4bda5983a",
+    "url": "/react-ant/static/media/Screenshot from 2018-12-18 11-55-13.b9deea1e.png"
+  },
+  {
+    "revision": "587c93173995eaad5ef03b3f8e49c112",
+    "url": "/react-ant/static/media/background.587c9317.jpeg"
+  }
+]);
